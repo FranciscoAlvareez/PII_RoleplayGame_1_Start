31bf09c5ce8bf7c5b058f78bf207fd3c82214006
belenferreir <beluferrerey@gmail.com>
2023-05-15 16:56:33 -0300

    B

--- src/Test/Library.Test/WizardsTest.cs
+++ src/Test/Library.Test/WizardsTest.cs
@@ -1,6 +1,5 @@
 using NUnit.Framework;
 
-
 namespace RoleplayGame
 {
     public class WizardsTest
@@ -10,7 +9,8 @@ namespace RoleplayGame
         {
             Wizard Wizard1 = new Wizard("Dr. Strange", 100);
             Wizard Wizard2 = new Wizard("Dr. Doom", 100);
-            ItemMagic Object = ItemMagic("Libro", "El Poder del Fuego", 50, 0);  // Me da error. Dice que ItemMagic no es invocable. No se si me da problema el Namespace en el archivo de ItemMagic
+            ItemMagic Object = new ItemMagic("Libro", "El Poder del Fuego", 50, 0);  
+            
             Wizard1.AddObjects(Object);
             Wizard1.Attack(Wizard2);
 
@@ -19,21 +19,3 @@ namespace RoleplayGame
     }
 }
 
-/*
-        public void  MagoTesteado()
-        {
-        Mago newmago = new Mago("Merlin");
-        Mago secondmago = new Mago("Oscurus");
-        Assert.AreEqual(60, newmago.Vida );
-        newmago.Atacar(newmago);
-        Assert.AreEqual(-34, newmago.Vida );
-
-        LibroDeMagia libroDeMagia = new LibroDeMagia();
-        newmago.EquiparMagia(libroDeMagia);
-        Assert.AreEqual(180, newmago.Daño);
-
-        newmago.Tempestad();
-        Assert.AreEqual(360, newmago.Daño);
-        }  
-           
-*/
