2728d3a7f5c11fd57eb552061e2a5c21281db80c
CabreraClau <claudio.cabrera@correo.ucu.edu.uy>
2023-05-15 17:03:00 -0300

    clau

--- src/Test/Library.Test/WizardsTest.cs
+++ src/Test/Library.Test/WizardsTest.cs
@@ -10,7 +10,7 @@ namespace RoleplayGame
         {
             Wizard Wizard1 = new Wizard("Dr. Strange", 100);
             Wizard Wizard2 = new Wizard("Dr. Doom", 100);
-            ItemMagic Object = ItemMagic("Libro", "El Poder del Fuego", 50, 0);  // Me da error. Dice que ItemMagic no es invocable. No se si me da problema el Namespace en el archivo de ItemMagic
+            ItemMagic Object = new ItemMagic("Libro", "El Poder del Fuego", 50, 0);  // Me da error. Dice que ItemMagic no es invocable. No se si me da problema el Namespace en el archivo de ItemMagic
             Wizard1.AddObjects(Object);
             Wizard1.Attack(Wizard2);
 
